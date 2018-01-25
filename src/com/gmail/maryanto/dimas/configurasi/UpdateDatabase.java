ce6f5cf8fbe620d2af9a0b417e8988cd83118765
dimmaryanto93 <engineer.dimmaryanto93@outlook.com>
2018-01-25 19:01:36 +0700

    update materi insert update delete

--- src/com/gmail/maryanto/dimas/configurasi/UpdateDatabase.java
+++ src/com/gmail/maryanto/dimas/configurasi/UpdateDatabase.java
@@ -4,14 +4,12 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 import org.apache.commons.dbcp2.BasicDataSource;
 
 import com.gmail.maryanto.dimas.model.Department;
 
-public class KoneksiKeDatabase {
-
+public class UpdateDatabase {
 	public static void main(String[] args) {
 		Integer value = 10;
 		BasicDataSource ds = new BasicDataSource();
@@ -24,23 +22,15 @@ public class KoneksiKeDatabase {
 			Connection connection = ds.getConnection();
 			System.out.println("koneksi database berhasil!");
 
-			PreparedStatement st = connection.prepareStatement(
-					"select * from departments where "
-					+ "department_id = ? and department_name = ?"
-			);
-			st.setInt(1, value);
+			PreparedStatement st = connection.prepareStatement("insert into departments"
+					+ "(department_id, department_name, location_id, manager_id) " +
+					"values (?, ?, ? ,?)");
+			st.setInt(1, 999);
 			st.setString(2, "aslkfjaskdl");
-			ResultSet rs = st.executeQuery();
-			while (rs.next()) {
-				Department d = new Department();
-				d.setDepartmentId(rs.getInt("DEPARTMENT_ID"));
-				d.setDepartmentName(rs.getString("DEPARTMENT_NAME"));
-				d.setLocationId(rs.getInt("MANAGER_ID"));
-				d.setManagerId(rs.getInt("LOCATION_ID"));
-				
-				System.out.println(d.toString());
-			}
-			rs.close();
+			st.setInt(3, 3200);
+			st.setInt(4, 124);
+			st.executeUpdate();
+
 			st.close();
 			connection.close();
 		} catch (SQLException e) {
@@ -48,5 +38,4 @@ public class KoneksiKeDatabase {
 			e.printStackTrace();
 		}
 	}
-
 }

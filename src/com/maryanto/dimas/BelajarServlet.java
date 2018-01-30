a3705052801955702c6a24135cf711f6b9a090e5
dimmaryanto93 <engineer.dimmaryanto93@outlook.com>
2018-01-30 17:05:44 +0700

    membuat response dan request

---
+++ src/com/maryanto/dimas/BelajarServlet.java
@@ -0,0 +1,36 @@
+package com.maryanto.dimas;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = "/halo")
+public class BelajarServlet extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) 
+			throws ServletException, IOException {
+		// http request, digunakan untuk kita (server) menambil permintaan dari si browser
+		String nama = req.getParameter("nama");
+		String nik = req.getParameter("nik");
+
+		// http response, digunakan untuk kita (server) mengeluarkan / menghasilkan html
+		resp.getWriter().append("halo nama saya dari method get " + nama + 
+				" dan niknya adalah " + nik);
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String nama = req.getParameter("nama");
+		String nik = req.getParameter("nik");
+
+		// http response, digunakan untuk kita (server) mengeluarkan / menghasilkan html
+		resp.getWriter().append("halo nama saya dari method post" + nama + 
+				" dan niknya adalah " + nik);
+	}
+
+}
